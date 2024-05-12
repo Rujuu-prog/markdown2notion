3909431f40a1588936a67bdafc9972f758e983ed
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2024-05-13 02:29:31 +0900

    :sparkles: add jest coverage config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,16 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js'],
-  testMatch: ['<rootDir>/__tests__/**/*.+(ts|js)']
+  testMatch: ['<rootDir>/__tests__/**/*.+(ts|js)'],
+  collectCoverage: true,  // カバレッジデータを収集する
+  coverageDirectory: 'coverage',  // カバレッジレポートの出力先ディレクトリ
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],  // カバレッジレポートの形式
+  coverageThreshold: {  // カバレッジの閾値設定
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 }
