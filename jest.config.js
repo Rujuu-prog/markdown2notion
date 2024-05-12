60bb121dda5faf2f9171e6a39d1ec81fefbbc471
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2024-05-12 14:20:01 +0900

    :wrench: add jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['<rootDir>/__tests__/**/*.+(ts|js)']
+}
