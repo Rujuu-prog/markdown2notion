531f897b9eb02f21406cd05768d60650c11e8346
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2025-04-14 01:34:57 +0900

    :coffin: remove dist for gitignore

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { markdownToNotion } from "./markdownToNotion";
-export { searchPage } from "./searchPage";
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchPage = exports.markdownToNotion = void 0;
-var markdownToNotion_1 = require("./markdownToNotion");
-Object.defineProperty(exports, "markdownToNotion", { enumerable: true, get: function () { return markdownToNotion_1.markdownToNotion; } });
-var searchPage_1 = require("./searchPage");
-Object.defineProperty(exports, "searchPage", { enumerable: true, get: function () { return searchPage_1.searchPage; } });
-//# sourceMappingURL=index.js.map
